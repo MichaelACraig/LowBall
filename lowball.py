c83099af803f2aa4829861814aca1d6bb00ea65e
michaelmacraig <michaelmacraig@gmail.com>
2024-10-17 21:42:19 -0500

    Added search_item feature and working on location setting. Headless acting funny

--- lowball.py
+++ lowball.py
@@ -1,12 +1,25 @@
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
-import time, os
+import pyautogui, time, os
 
 # Chrome Webdriver Initalizer
 def connect(url='https://www.facebook.com/marketplace'):
-  driver = webdriver.Chrome()
+  
+  # I don't know why, but headless mode opens a white box when it shouldn't be opening anything
+  # Fix later!
+  chrome_options = Options()
+  #chrome_options.add_argument("--headless")
+  #chrome_options.add_argument("--disable-gpu")
+  #chrome_options.add_argument("--no-sandbox")
+  chrome_options.add_argument("--window-size=1920x1080")
+
+  driver = webdriver.Chrome(options=chrome_options)
   driver.get(url)
   
   return driver
@@ -32,11 +45,59 @@ def input_user_credentials(driver):
   pass_field.send_keys(password)
 
   login.click()
-  time.sleep(100)
+  
+  # Add WebDriverWait sometime later when we finnd a good login element
+  # Can't find one right now so good temp fix
+  time.sleep(10)
+
+  cookies = driver.get_cookies()
+
+  return driver, cookies
+
+# Searches an for an item given it finds a search bar
+def search_item(driver, item, cookies=None):
+  search_url = 'https://www.facebook.com/marketplace/category/search/?query='
+  item_split = item.split(" ")
+  
+  # URL Edit for search
+  if len(item_split) == 1:
+      search_url += i
+  else:
+    for i in item_split:
+        if i == item_split[0]:
+           search_url += i
+        else:
+            search_url += f'%{i}'
+  
+  driver.get(search_url) 
+  
+  return driver
+
+# Go to specific location
+def define_location(driver, cookies=None):
+  screen_width, screen_height = pyautogui.size()
+  # Random coordinates on webpage to remove Notifications blocker
+  x = 627
+  y = 250
+
+  pyautogui.moveTo(x, y)
+  pyautogui.click()
+
+  location_change = driver.find_element(By.XPATH, '//*[@id="seo_filters"]/div[1]/div[1]/div/span')
+  location_change.click()
+
+  return driver
+
+# Fully encompassing function that adjusts all features for item search 
+def apply_filters(driver):
   return driver
 
 def main():
   driver = connect()
-  driver = input_user_credentials(driver)
+  driver, cookies = input_user_credentials(driver)
+  driver = search_item(driver, "Fortnite Battle Pass")
+  driver = define_location(driver)
+  time.sleep(10)
+  driver.quit()
 
 main()
