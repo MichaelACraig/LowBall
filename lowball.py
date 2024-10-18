ea0b5b14cd04479d96a38dcfdd15262d70bbd2e1
michaelmacraig <michaelmacraig@gmail.com>
2024-10-18 00:12:56 -0500

    Added feature to scrape listings

--- lowball.py
+++ lowball.py
@@ -1,12 +1,13 @@
 from dotenv import load_dotenv
 from selenium import webdriver
+from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
-import pyautogui, time, os
+import pyautogui, time, os, json
 
 # Chrome Webdriver Initalizer
 def connect(url='https://www.facebook.com/marketplace'):
@@ -27,26 +28,25 @@ def connect(url='https://www.facebook.com/marketplace'):
 # Helper for input_user_credentials to ennsure input fields are found.
 def retrieve_input_fields(driver):
    try:
-    user_field = driver.find_element(By.XPATH, '//*[@id=":r1:"]')
-    pass_field = driver.find_element(By.XPATH, '//*[@id=":r4:"]')
-    login = driver.find_element(By.XPATH, '//*[@id="login_popup_cta_form"]/div/div[5]')
+    user_field = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id=":r1:"]')))
+    pass_field = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id=":r4:"]')))
+    login = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="login_popup_cta_form"]/div/div[5]')))
 
     return user_field, pass_field, login
    except:
       return None, None, None
    
-# Input login credentials to Facebook Marketplace; Technically don't need to do this
-# Simple secondary check. Shouldn't be an issue unless you have bad internet
+# Input login credentials to Facebook Marketplace; Technically don't need to do this; Simple secondary check. Shouldn't be an issue unless you have bad internet
 def input_user_credentials(driver):
   load_dotenv()
   username = os.getenv('USERNAME')
   password = os.getenv('PASSWORD')
+
+  # Need some sort of clause for if the username and password are not present
   
   user_field, pass_field, login = retrieve_input_fields(driver)
   if user_field is None:
     retrieve_input_fields(driver)
-  
-  # Need some sort of clause for if the username and password are not present
   
   user_field.click()
   user_field.send_keys(username)
@@ -55,10 +55,6 @@ def input_user_credentials(driver):
   pass_field.send_keys(password)
 
   login.click()
-  
-  # Add WebDriverWait sometime later when we finnd a good login element
-  # Can't find one right now so good temp fix
-  time.sleep(10)
 
   cookies = driver.get_cookies()
 
@@ -83,8 +79,39 @@ def search_item(driver, item, cookies=None):
   
   return driver
 
-# Go to specific location
-def define_location(driver, radius=None, city=None, state=None, zipcode=None, cookies=None):
+# Scrape listings and collect the necessary data
+def scrape_listings(driver):
+  soup = BeautifulSoup(driver.page_source, 'html.parser')
+  listings = soup.find_all('div',class_='x9f619 x78zum5 x1r8uery xdt5ytf x1iyjqo2 xs83m0k x1e558r4 x150jy0e x1iorvi4 xjkvuk6 xnpuxes x291uyu x1uepa24')
+  listing_data = []
+  for listing in listings:
+    data = {}
+    link_layer = listing.find('a', class_='x1i10hfl xjbqb8w x1ejq31n xd10rxx x1sy0etr x17r0tee x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz x1heor9g x1sur9pj xkrqix3 x1lku1pv')
+    if link_layer and 'href' in link_layer.attrs:
+      href = link_layer['href']
+      link = f'https://www.facebook.com{href}'
+      data['link'] = link
+    
+    price = soup.find('span', class_='x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x xudqn12 x676frb x1lkfr7t x1lbecb7 x1s688f xzsf02u')
+    price_text = price.text
+    data['price'] = price_text
+
+    layer = soup.find('div', class_='xyqdw3p x4uap5 xjkvuk6 xkhd6sd')
+    name = layer.find('span', class_='x1lliihq x6ikm8r x10wlt62 x1n2onr6')
+    name_text = name.text
+    data['name'] = name_text
+
+    location = soup.find('span', class_='x1lliihq x6ikm8r x10wlt62 x1n2onr6 xlyipyv xuxw1ft x1j85h84')
+    location_text = location.text
+    data['location'] = location_text
+
+    listing_data.append(data)
+
+    return listing_data
+    
+# Go to specific location (FUNCTION CANNOT BE HEADLESS IN ORDER TO RUN)
+#WIP
+def define_location(driver, zipcode=None, radius=None, cookies=None):
   # Random coordinates on webpage to remove Notifications blocker
   screen_width, screen_height = pyautogui.size()
   x = 627
@@ -92,12 +119,26 @@ def define_location(driver, radius=None, city=None, state=None, zipcode=None, co
   pyautogui.moveTo(x, y)
   pyautogui.click()
   
-  time.sleep(1)
-  location_change = driver.find_element(By.XPATH, '//*[@id="seo_filters"]/div[1]/div[1]/div/span')
+  location_change = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="seo_filters"]/div[1]/div[1]/div/span')))
   location_change.click()
 
-  location_search = driver.find_element(By.XPATH, '''//*[@id=":r19:"]''')
-  location_search.click()
+  # Change radius around location (WIP Doesn't work right now)
+  if radius is not None:
+    radius_menu = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="mount_0_0_XL"]/div/div[1]/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div[3]/div/div[1]/div[3]/div/div/div')))
+    radius_menu.click()
+    time.sleep(10)
+
+  location_search_x = 600
+  location_search_y = 550
+  pyautogui.moveTo(location_search_x, location_search_y)
+  time.sleep(1)
+  pyautogui.click()
+  
+  if zipcode is not None:
+     pyautogui.typewrite(zipcode)
+  
+  pyautogui.press('enter')
+
   return driver
 
 # Applies all filters that user has put parameters in for
@@ -108,7 +149,7 @@ def main():
   driver = connect()
   driver, cookies = input_user_credentials(driver)
   driver = search_item(driver, "Fortnite Battle Pass")
-  driver = define_location(driver)
+  data = scrape_listings(driver)
   time.sleep(10)
   driver.quit()
 
