9ef120cb8bcc413ad29999becb916ca7b3762069
michaelmacraig <michaelmacraig@gmail.com>
2025-01-25 15:25:31 -0500

    Fixed silly buttons

--- lowball.py
+++ lowball.py
@@ -96,7 +96,7 @@ def scrape_listings(driver):
       data['link'] = ''
     
     try: # Issue with this one for slashed out prices due to discounts
-      price = listing.find('span', class_='x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x xudqn12 x676frb x1lkfr7t x1lbecb7 x1s688f xzsf02u')
+      price = listing.find('span', class_='x193iq5w xeuugli x13faqbe x1vvkbs xlh3980 xvmahel x1n0sxbx x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x x4zkp8e x3x7a5m x1lkfr7t x1lbecb7 x1s688f xzsf02u')
       price_text = price.text
       data['price'] = price_text
     except:
@@ -111,7 +111,7 @@ def scrape_listings(driver):
       data['name'] = ''
 
     try:
-      location = listing.find('span', class_='x1lliihq x6ikm8r x10wlt62 x1n2onr6 xlyipyv xuxw1ft x1j85h84')
+      location = listing.find('span', class_='x1lliihq x6ikm8r x10wlt62 x1n2onr6 xlyipyv xuxw1ft')
       location_text = location.text
       data['location'] = location_text
     except:
